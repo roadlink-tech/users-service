0bbc298b9c6b803205e8f4421b4806438486ef11
jorgecabrera-pedidosya <jorge.cabrera@pedidosya.com>
2019-09-18 15:43:51 -0300

    spring data jpa auditing

---
+++ src/main/kotlin/com/peya/usersservice/config/JpaAuditingConfiguration.kt
@@ -0,0 +1,18 @@
+package com.peya.usersservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.AuditorAware
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.util.*
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+class JpaAuditingConfiguration {
+
+    @Bean
+    fun auditorProvider(): AuditorAware<String> {
+        return AuditorAware { Optional.of(" ") }
+    }
+
+}
--- src/main/kotlin/com/peya/usersservice/model/User.kt
+++ src/main/kotlin/com/peya/usersservice/model/User.kt
@@ -1,12 +1,16 @@
 package com.peya.usersservice.model
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.*
 import javax.persistence.GenerationType.IDENTITY
-import javax.persistence.Id
 import javax.validation.constraints.Email
 
 @Entity
+@Table(name = "user")
+@EntityListeners(AuditingEntityListener::class)
 data class User(
         @Id @GeneratedValue(strategy = IDENTITY)
         val id: Long = 0,
@@ -16,5 +20,13 @@ data class User(
         val lastName: String = "",
 
         @Email
-        val email: String = ""
+        val email: String = "",
+
+        @CreatedDate
+        @Column(name = "created_date", nullable = false, updatable = false)
+        var createdDate: LocalDateTime? = null,
+
+        @LastModifiedDate
+        @Column(name = "last_modified_date", nullable = false)
+        var lastModifiedDate: LocalDateTime? = null
 )
