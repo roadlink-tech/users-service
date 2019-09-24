19ff13a0a45e63f3308af403cde37ec15ec68998
jorgecabrera-pedidosya <jorge.cabrera@pedidosya.com>
2019-09-24 15:03:06 -0300

    add logs

--- src/main/kotlin/com/peya/usersservice/service/UserService.kt
+++ src/main/kotlin/com/peya/usersservice/service/UserService.kt
@@ -4,12 +4,19 @@ import com.peya.usersservice.dto.UserDto
 import com.peya.usersservice.exception.ResourceNotFound
 import com.peya.usersservice.model.User
 import com.peya.usersservice.repository.UserRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(private val userRepository: UserRepository) {
 
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = LoggerFactory.getLogger(javaClass.enclosingClass)
+    }
+
     fun getUser(id: Long): User {
+        logger.info("Retrieving user $id.")
         return userRepository.findById(id).orElseThrow { throw ResourceNotFound("User $id does not exists.") }
     }
 
