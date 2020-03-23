84a809040fa16df6131f3dccc927935a22f4c270
jorgecabrera-pedidosya <jorge.cabrera@pedidosya.com>
2020-03-22 22:16:14 -0300

    auth rest client

--- src/main/kotlin/com/peya/usersservice/infrastructure/AuthRestClient.kt
+++ src/main/kotlin/com/peya/usersservice/infrastructure/AuthRestClient.kt
@@ -1,10 +1,22 @@
 package com.peya.usersservice.infrastructure
 
 import com.peya.usersservice.domain.repository.AuthRepository
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.web.client.RestTemplate
+import java.net.URI
 
 class AuthRestClient : AuthRepository {
 
     override fun saveAuthRegistrationInfo(pwd: String, email: String, phone: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var client = RestTemplate()
+        val headers = HttpHeaders()
+        val url = "localhost:8081/register"
+        val uri = URI(url)
+        val body = AuthBody(userId = 1, password = pwd)
+        headers.contentType = APPLICATION_JSON
+        client.postForEntity(uri, body, String::class.java)
     }
 }
+
+data class AuthBody(val userId: Long, val password: String)
